36b1844ac1ab74519d4e22bb0c91f1a41d0057e3
mohammadshihabudeen <city.shihabudeen@gmail.com>
2025-02-01 23:39:45 +0530

    changes

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import SignInForm from "@/components/SignInForm";
 
 export default function SignupPage() {
